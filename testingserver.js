0129beb8265bdd0bd5c5ec6364b75f65be115abe
Sydnie Chau <sydniechau@Sydnies-MacBook-Pro.local>
2020-05-29 22:29:51 -0400

    added update list feature

--- testingserver.js
+++ testingserver.js
@@ -1,17 +1,16 @@
 var express = require("express");
-
 var bodyParser = require("body-parser");
-// var cors = require("cors");
-// const Mongodb = require("mongodb");
+
 var db;
 var currentRoom = "";
 //import puppetter
 const puppeteer = require("puppeteer");
 var urls = [
   "https://www.amazon.ca/gp/product/B07VG7PMC5/ref=ppx_yo_dt_b_asin_title_o06_s00?ie=UTF8&psc=1",
-  "!",
+  "https://www.amazon.ca/REDMOND-Toaster-Defrost-Functions-Removable/dp/B07V3R1PR2?pf_rd_r=EFRX7Z7DB58CQSMB4ECH&pf_rd_p=49ee6bf9-5967-4f3d-afea-1e0e663e4324&pd_rd_r=3f7f817a-2392-43fb-baa5-e61cca1fcb94&pd_rd_w=KGcQT&pd_rd_wg=dc41x&ref_=pd_gw_hlp13n_t4im",
 ];
-
+//https://www.amazon.ca/gp/product/B07VG7PMC5/ref=ppx_yo_dt_b_asin_title_o06_s00?ie=UTF8&psc=1
+var updatedList = [];
 var app = express();
 
 var server = app.listen(3000, function () {
@@ -28,12 +27,29 @@ app.use(bodyParser.json());
 
 //posts request for add item url
 app.post("/additem", function (request, response) {
-  console.log(request.body);
+  // console.log(request.body);
   var url = request.body.myurl;
-  console.log(request.body.url);
-  console.log(url);
+  // console.log(request.body.url);
+  // console.log(url);
   urls.push(url);
   console.log(urls);
+  (async () => {
+    var values = await scrapeProduct(url);
+    var obj = [
+      {
+        userId: 1,
+        id: 1,
+        title: values[0],
+        body: values[1],
+      },
+    ];
+    response.writeHead(200, {
+      "Content-Type": "application/json; charset=utf-8",
+    });
+    response.write(JSON.stringify(obj));
+    response.end();
+    console.log("sending post back");
+  })();
 });
 
 //gets a list of current products
@@ -48,35 +64,57 @@ app.get("/testing", function (request, response) {
   //   status: { type: type, message: message, code: code, error: error },
   //   data: { chatrooms: "pee" },
   // };
-  updateList(urls);
-  (async () => {
-    var values = await scrapeProduct(
-      "https://www.amazon.ca/gp/product/B07VG7PMC5/ref=ppx_yo_dt_b_asin_title_o06_s00?ie=UTF8&psc=1"
-    );
-    console.log(values);
-    console.log("title" + values[0]);
-    console.log("prices" + values[1]);
-    var obj = [
-      {
+  var updatedListnew = updatelistfunction(urls, updatedList);
+  response.writeHead(200, {
+    "Content-Type": "application/json; charset=utf-8",
+  });
+  response.write(JSON.stringify(updatedListnew));
+  response.end();
+  console.log("sending get request back" + updatedList);
+
+  // (async () => {
+  //   var values = await scrapeProduct(
+  //     "https://www.amazon.ca/gp/product/B07VG7PMC5/ref=ppx_yo_dt_b_asin_title_o06_s00?ie=UTF8&psc=1"
+  //   );
+  //   // console.log(values);
+  //   // console.log("title" + values[0]);
+  //   // console.log("prices" + values[1]);
+  //   var obj = [
+  //     {
+  //       userId: 1,
+  //       id: 1,
+  //       title: values[0],
+  //       body: values[1],
+  //     },
+  //   ];
+  //   response.writeHead(200, {
+  //     "Content-Type": "application/json; charset=utf-8",
+  //   });
+  //   response.write(JSON.stringify(obj));
+  //   response.end();
+  //   console.log("sending get request back");
+  // })();
+});
+//function to retreive updated title and prices of url
+function updatelistfunction(urls, updatedList) {
+  for (i = 0; i < urls.length; i++) {
+    (async () => {
+      // console.log(urls);
+      var values = await scrapeProduct(urls[i]);
+      console.log("values" + values);
+      updatedList.push({
         userId: 1,
         id: 1,
         title: values[0],
         body: values[1],
-      },
-    ];
-    response.writeHead(200, {
-      "Content-Type": "application/json; charset=utf-8",
-    });
-    response.write(JSON.stringify(obj));
-    response.end();
-    console.log("sending");
-  })();
-});
-//function to retreive updated title and prices of url
-function updateList(urls) {
-  for (i = 0; i < urls.length; i++) {
-    console.log(i);
+      });
+
+      // console.log(values);
+      // console.log("title" + values[0]);
+      // console.log("prices" + values[1]);
+    })();
   }
+  return updatedList;
 }
 //function for retreiving title and price of product from single url
 async function scrapeProduct(url) {
