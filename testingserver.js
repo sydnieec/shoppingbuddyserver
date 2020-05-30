28ab326ffc3d3c56e96e559f60072da87b16ddec
Sydnie Chau <sydniechau@Sydnies-MacBook-Pro.local>
2020-05-29 20:56:47 -0400

    post

--- testingserver.js
+++ testingserver.js
@@ -1,10 +1,16 @@
 var express = require("express");
+
 var bodyParser = require("body-parser");
 // var cors = require("cors");
 // const Mongodb = require("mongodb");
 var db;
 var currentRoom = "";
-// const puppeteer = require("puppeteer");
+//import puppetter
+const puppeteer = require("puppeteer");
+var urls = [
+  "https://www.amazon.ca/gp/product/B07VG7PMC5/ref=ppx_yo_dt_b_asin_title_o06_s00?ie=UTF8&psc=1",
+  "!",
+];
 
 var app = express();
 
@@ -20,6 +26,17 @@ app.use(
 app.use(bodyParser.json());
 // app.use(cors());
 
+//posts request for add item url
+app.post("/additem", function (request, response) {
+  console.log(request.body);
+  var url = request.body.myurl;
+  console.log(request.body.url);
+  console.log(url);
+  urls.push(url);
+  console.log(urls);
+});
+
+//gets a list of current products
 app.get("/testing", function (request, response) {
   // console.log(res);
   var error, type, message, code;
@@ -31,55 +48,87 @@ app.get("/testing", function (request, response) {
   //   status: { type: type, message: message, code: code, error: error },
   //   data: { chatrooms: "pee" },
   // };
-
-  var obj = [
-    {
-      userId: 1,
-      id: 1,
-      title: "chicken nuggests",
-      body: "$5",
-    },
-  ];
-  response.writeHead(200, {
-    "Content-Type": "application/json; charset=utf-8",
-  });
-  response.write(JSON.stringify(obj));
-  response.end();
-  console.log("sending");
+  updateList(urls);
+  (async () => {
+    var values = await scrapeProduct(
+      "https://www.amazon.ca/gp/product/B07VG7PMC5/ref=ppx_yo_dt_b_asin_title_o06_s00?ie=UTF8&psc=1"
+    );
+    console.log(values);
+    console.log("title" + values[0]);
+    console.log("prices" + values[1]);
+    var obj = [
+      {
+        userId: 1,
+        id: 1,
+        title: values[0],
+        body: values[1],
+      },
+    ];
+    response.writeHead(200, {
+      "Content-Type": "application/json; charset=utf-8",
+    });
+    response.write(JSON.stringify(obj));
+    response.end();
+    console.log("sending");
+  })();
 });
+//function to retreive updated title and prices of url
+function updateList(urls) {
+  for (i = 0; i < urls.length; i++) {
+    console.log(i);
+  }
+}
+//function for retreiving title and price of product from single url
+async function scrapeProduct(url) {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
 
-// var express = require("express");
-// var router = express.Router();
+    const [el] = await page.$x('//*[@id="productTitle"]');
+    const txt = await el.getProperty("textContent");
+    var title = await txt.jsonValue();
 
-// //GET HomePage
-// router /
-//   getComputedStyle("/", function (req, res, next) {
-//     res.render("index", {
-//       title: "Cool, huh?",
-//       condition: true,
-//       anyArray: [1, 2, 3],
-//     });
-//   });
+    const [el2] = await page.$x('//*[@id="price_inside_buybox"]');
+    const txt2 = await el2.getProperty("textContent");
+    var price = await txt2.jsonValue();
 
-// module.exports = router;
+    //strips the title and price of whitespaces and new lines
+    var title = title.replace(/\n/g, " ");
+    var price = price.replace(/\n/g, " ");
+    var title = title.replace(/\s\s+/g, " ");
+    var price = price.replace(/\s\s+/g, " ");
 
-// var express = require("express");
+    // console.log({ title, price });
 
-// var app = express();
+    browser.close();
 
-// var PORT = 3000;
+    return await [title, price];
+  } catch (error) {
+    //catches error if the price is on another div
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
 
-// app.get("/", function (req, res) {
-//   res.status(200).send("Hello world");
-// });
+    const [el] = await page.$x('//*[@id="productTitle"]');
+    const txt = await el.getProperty("textContent");
+    var title = await txt.jsonValue();
 
-// app.listen(PORT, function () {
-//   console.log("Server is running on PORT:", PORT);
-// });
-// //new
-// var http = require("http");
+    const [el2] = await page.$x('//*[@id="priceblock_dealprice"]');
+    const txt2 = await el2.getProperty("textContent");
+    var price = await txt2.jsonValue();
 
-// var server = http.createServer(function (req, res) {
-//   res.writeHead(200, { "Content-type": "text/plain" });
-//   res.end("Hello world\n");
-// });
+    //strips the title and price of whitespaces and new lines
+    var title = title.replace(/\n/g, " ");
+    var price = price.replace(/\n/g, " ");
+    var title = title.replace(/\s\s+/g, " ");
+    var price = price.replace(/\s\s+/g, " ");
+    // console.log({ title, price });
+
+    browser.close();
+    // console.log({ title, price });
+
+    return await [title, price];
+    //console.log("price in priceblock");
+  }
+}
